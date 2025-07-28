d56a6cff47cd9f470b739f031cfcc6c7cd7cbef0
binit <binitgupta.1711@gmail.com>
2025-07-28 22:10:33 +0530

    Chevron Functionality added, It now collapses and expands and shows the content.

--- src/components/reusables/CollapseBox.jsx
+++ src/components/reusables/CollapseBox.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import '../../styles/CollapseBox.css';
 import { ChevronDownIcon } from '@heroicons/react/16/solid';
+import { useState } from 'react';
 
-const CollapseBox = ({ title, content }) => {
+
+const CollapseBox = ({ title, content = "Test-Text"}) => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="collapse-box">
       <div className="collapse-header">
         <h1>{title}</h1>
-        <ChevronDownIcon className="chevron-icon" />
+        <ChevronDownIcon className={`chevron-icon ${isOpen ? 'open' : ''}`} onClick={toggleCollapse} />
       </div>
       <div className="collapse-content">
-        {content}
+        {isOpen && content}
       </div>
     </div>
   )
