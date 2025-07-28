04701c14cf948a727999a6112d138fddfec6af4d
binit <binitgupta.1711@gmail.com>
2025-07-28 21:58:44 +0530

    chevron added and placecment fixed

--- src/components/reusables/CollapseBox.jsx
+++ src/components/reusables/CollapseBox.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import '../../styles/CollapseBox.css';
+import { ChevronDownIcon } from '@heroicons/react/16/solid';
 
 const CollapseBox = ({ title, content }) => {
   return (
     <div className="collapse-box">
-      <h2>{title}</h2>
+      <div className="collapse-header">
+        <h1>{title}</h1>
+        <ChevronDownIcon className="chevron-icon" />
+      </div>
       <div className="collapse-content">
         {content}
       </div>
-    </div> 
+    </div>
   )
 }
 
