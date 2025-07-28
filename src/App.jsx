61fbbbe0dfa17e3c44410aace716268b9388bf9a
binit <binitgupta.1711@gmail.com>
2025-07-28 21:38:23 +0530

    Collapse box  component styles updaetd now box is getting rendreed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import PersonalDetails from './components/PersonalDetails.jsx';
+import CollapseBox from './components/reusables/CollapseBox.jsx';
 import './App.css'
 
 const App = () => {
   return (
      <div className="parent">
-      <div className="div1"> <PersonalDetails /> </div>
+      <div className="div1"> 
+        <PersonalDetails />
+        <CollapseBox title={<h1>Education</h1>} />
+      </div>
       <div className="div2"> </div>
    </div>
   );
--- src/components/reusables/CollapseBox.jsx
+++ src/components/reusables/CollapseBox.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../../styles/CollapseBox.css';
 
 const CollapseBox = ({ title, content }) => {
   return (
