0bd295a54cc2464958e0da7dae69b16b6a31ff9b
binit <binitgupta.1711@gmail.com>
2025-07-28 22:19:12 +0530

    The input fields are correctly setup

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,16 @@ const App = () => {
      <div className="parent">
       <div className="div1"> 
         <PersonalDetails />
-        <CollapseBox title={<h1>Education</h1>} />
+        <CollapseBox title={<h1>Education</h1>} 
+        content = {<form>
+          <label className='label-pd'>Institution:</label>
+          <input type="text" placeholder='e.g. XYZ International' className='input-pd' />
+          <label className='label-pd'>Degree:</label>
+          <input type="text" placeholder='e.g. Bachelor of Science' className='input-pd' />
+          <label className='label-pd'>Year of Graduation:</label>
+          <input type="text" placeholder='e.g. 2020' className='input-pd' />
+        </form>} 
+        />
       </div>
       <div className="div2"> </div>
    </div>
