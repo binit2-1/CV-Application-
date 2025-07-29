b7353cdbeda312102df11426999d936e492a6196
binit <binitgupta.1711@gmail.com>
2025-07-30 00:44:56 +0530

    Refactor CollapseBox component to manage height state for smooth transitions and update CSS for better collapsible behavior.

--- src/components/CollapseBox.jsx
+++ src/components/CollapseBox.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
 import '../styles/CollapseBox.css';
 import { ChevronDownIcon } from '@heroicons/react/16/solid';
-import { useState } from 'react';
 
 
 const CollapseBox = ({ title, content = "Test-Text"}) => {
 
   const [isOpen, setIsOpen] = useState(false);
+  const [height, setHeight] = useState(0);
+  const contentRef = useRef(null);
 
   const toggleCollapse = () => {
-    setIsOpen(!isOpen);
+    if (!isOpen) {
+      setHeight(contentRef.current.scrollHeight);
+      setIsOpen(true);
+    } else {
+      setHeight(contentRef.current.scrollHeight); 
+      setTimeout(() => {
+        setHeight(0);
+        setIsOpen(false);
+      }, 10);
+    }
   };
 
   return (
@@ -18,8 +28,12 @@ const CollapseBox = ({ title, content = "Test-Text"}) => {
         <h1>{title}</h1>
         <ChevronDownIcon className={`chevron-icon ${isOpen ? 'open' : ''}`} onClick={toggleCollapse} /> 
       </div>
-      <div className="collapse-content">
-        {isOpen && content}
+      <div
+        className={`collapse-content${isOpen ? ' open' : ''}`}
+        style={{ maxHeight: `${isOpen ? height : 0}px` }}
+        ref={contentRef}
+      >
+        {content}
       </div>
     </div>
   )
