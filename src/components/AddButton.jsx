be426b4f5bb9c61f5c7233825650ca1aab6be738
binit <binitgupta.1711@gmail.com>
2025-08-03 23:17:09 +0530

    Add AddButton component for dynamic button rendering

---
+++ src/components/AddButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AddButton = ({
+    buttonText
+}) => {
+  return (
+    <div>
+        <button className='add-button'>{buttonText}</button>
+    </div>
+  )
+}
+
+export default AddButton
